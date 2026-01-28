51d43e3b70f1959ce0d22254986c359dadc70f7d
yummyaom <guinjinch@gmail.com>
2026-01-29 02:47:38 +0700

    add .env

--- services/auth-service/src/main/java/com/growthsheet/user_service/UserServiceApplication.java
+++ services/auth-service/src/main/java/com/growthsheet/user_service/UserServiceApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+        .directory("../..")
+        .ignoreIfMissing()
+        .load();
+		dotenv.entries().forEach(
+			e -> System.setProperty(e.getKey(), e.getValue())
+		);
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
--- services/product-service/src/main/java/com/growthsheet/product_service/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/growthsheet/product_service/ProductServiceApplication.java
@@ -3,10 +3,19 @@ package com.growthsheet.product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+        .directory("../..")
+        .ignoreIfMissing()
+        .load();
+		dotenv.entries().forEach(
+			e -> System.setProperty(e.getKey(), e.getValue())
+		);
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
