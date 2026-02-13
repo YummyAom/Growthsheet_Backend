a4dc09a0fbbea3a658b99074adf67050878c58a7
yummyaom <guinjinch@gmail.com>
2026-02-13 13:48:54 +0700

    fix config 3

--- services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/ProductClient.java
+++ services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/ProductClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 // import com.growthsheet.order_service.dto.response.ProductResponse;
 
-@FeignClient(name = "product-service", url = "https://growthsheet-backend.onrender.com/products")
+@FeignClient(name = "product-service", url = "https://growthsheet-backend.onrender.com")
 public interface ProductClient {
     @GetMapping("/products/{id}") 
     ProductResponse getSheetById(@PathVariable("id") UUID id);
--- services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/UserClient.java
+++ services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.UUID;
 
-@FeignClient(name = "user-service", url = "https://growthsheet-backend.onrender.com/auth")
+@FeignClient(name = "user-service", url = "https://growthsheet-backend.onrender.com")
 public interface UserClient {
 
     @GetMapping("/auth/{id}")
