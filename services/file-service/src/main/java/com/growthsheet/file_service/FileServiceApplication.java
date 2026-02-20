5a3076fa42bc69e1aac90bf94b1c2d923fe67c9d
yummyaom <guinjinch@gmail.com>
2026-02-20 23:45:35 +0700

    add file-service

--- services/file-service/src/main/java/com/growthsheet/file_service/FileServiceApplication.java
+++ services/file-service/src/main/java/com/growthsheet/file_service/FileServiceApplication.java
@@ -3,10 +3,18 @@ package com.growthsheet.file_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FileServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("../..")
+				.ignoreIfMissing()
+				.load();
+		dotenv.entries().forEach(
+				e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(FileServiceApplication.class, args);
 	}
 
