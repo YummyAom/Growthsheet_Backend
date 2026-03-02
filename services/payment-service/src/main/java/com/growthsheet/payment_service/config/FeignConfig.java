d29fe5d95d669fdc6dda98b96083918c241f2f7c
yummyaom <guinjinch@gmail.com>
2026-03-02 20:55:13 +0700

    add getheader Feign

--- services/payment-service/src/main/java/com/growthsheet/payment_service/config/FeignConfig.java
+++ services/payment-service/src/main/java/com/growthsheet/payment_service/config/FeignConfig.java
@@ -2,6 +2,8 @@ package com.growthsheet.payment_service.config;
 
 import feign.RequestInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -10,19 +12,27 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Configuration
 public class FeignConfig {
 
+    @Value("${SERVICE_TOKEN}")
+    private String serviceToken;
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return template -> {
             ServletRequestAttributes attrs =
                     (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 
+            String authHeader = null;
+
             if (attrs != null) {
                 HttpServletRequest request = attrs.getRequest();
-                String authHeader = request.getHeader("Authorization");
+                authHeader = request.getHeader("Authorization");
+            }
 
-                if (authHeader != null) {
-                    template.header("Authorization", authHeader);
-                }
+            if (authHeader != null) {
+                template.header("Authorization", authHeader);
+            }
+            else {
+                template.header("Authorization", "Bearer " + serviceToken);
             }
         };
     }
