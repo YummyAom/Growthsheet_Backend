031ac795c510ccad68b6584c1c9c66b7cb973756
Pongpipat <pongpipatongart.m1@gmail.com>
2026-03-06 21:12:50 +0700

    fetch withdraw request

---
+++ services/admin-service/src/main/java/com/growthsheet/admin_service/repository/WithdrawalRequestRepository.java
@@ -0,0 +1,30 @@
+package com.growthsheet.admin_service.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.growthsheet.admin_service.entity.WithdrawStatus;
+import com.growthsheet.admin_service.entity.WithdrawalRequest;
+
+public interface WithdrawalRequestRepository extends JpaRepository<WithdrawalRequest, UUID> {
+
+    @Query(
+    value = """
+        SELECT wr FROM WithdrawalRequest wr
+        LEFT JOIN FETCH wr.sellerDetails
+        WHERE wr.status = :status
+        ORDER BY wr.createdAt DESC
+        """,
+    countQuery = """
+        SELECT count(wr) FROM WithdrawalRequest wr
+        WHERE wr.status = :status
+        """
+)
+Page<WithdrawalRequest> findByStatus(@Param("status") WithdrawStatus status, Pageable pageable);
+}
