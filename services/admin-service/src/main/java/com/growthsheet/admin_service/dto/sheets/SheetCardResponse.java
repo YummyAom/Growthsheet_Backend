51918f6a008723d411b37aaa8cb4df08d590aaf1
Pongpipat <pongpipatongart.m1@gmail.com>
2026-03-12 22:07:48 +0700

    fix sheetcard

--- services/admin-service/src/main/java/com/growthsheet/admin_service/dto/sheets/SheetCardResponse.java
+++ services/admin-service/src/main/java/com/growthsheet/admin_service/dto/sheets/SheetCardResponse.java
@@ -3,13 +3,15 @@ package com.growthsheet.admin_service.dto.sheets;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.growthsheet.admin_service.dto.SellerDTO;
 
 public record SheetCardResponse(
-        UUID id,
-        String title,
-        BigDecimal price,
-        SellerDTO seller,
-        String thumbnailUrl,
-        Boolean isPublished
-) {}
+                UUID id,
+                String title,
+                BigDecimal price,
+                SellerDTO seller,
+                @JsonProperty("image")
+                String thumbnailUrl,
+                Boolean isPublished) {
+}
