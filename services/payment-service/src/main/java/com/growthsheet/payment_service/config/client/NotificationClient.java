97574a80cced5bf32c01ae1b587d05bbbd2baa39
yummyaom <guinjinch@gmail.com>
2026-03-13 02:03:19 +0700

    fix notification client

--- services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/NotificationClient.java
+++ services/payment-service/src/main/java/com/growthsheet/payment_service/config/client/NotificationClient.java
@@ -11,7 +11,7 @@ import com.growthsheet.payment_service.dto.NotificationRequest;
 )
 public interface NotificationClient {
 
-    @PostMapping("/api/notifications")
+    @PostMapping("/notifications")
     void createNotification(
         @RequestBody NotificationRequest request
     );
